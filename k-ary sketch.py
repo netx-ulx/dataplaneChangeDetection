9b648ec43780d8e885c5074317c20e72e54186c5
Goncalo Matos <goncalo.o.matos@tecnico.ulisboa.pt>
2020-10-06 15:17:54 +0100

    added non-seasonal holt-winters forecasting model

--- k-ary sketch.py
+++ k-ary sketch.py
@@ -87,7 +87,7 @@ class KAry_Sketch:
         result = []
         for i in range(0,self.depth):
             bucket = mmh3.hash64(','.join(key),self.seeds[i])[0]%self.width
-            result.append((self.sketch[i][bucket] - (self.sum()/self.width)) / (1 - (1/self.width)))
+            result.append( (self.sketch[i][bucket] - (self.sum()/self.width)) / (1 - (1/self.width)))
         return median(result)
 
     def sum(self):
@@ -192,7 +192,7 @@ def SMA(forecast_sketch,sketch_list,w,weigths):
             new_forecast_sketch.sketch[i][j] = _sum / sum(weigths)
     return new_forecast_sketch
 
-def EWMA(forecast_sketch,observed_sketch,alpha):
+def EWMA(previous_forecast_sketch,previous_observed_sketch,alpha):
     """Uses the Exponentially Weighted Moving Average Model to build the forecast sketch from the previous forecast and observed sketch
 
     Parameters
@@ -210,16 +210,74 @@ def EWMA(forecast_sketch,observed_sketch,alpha):
         The forecast sketch
     """
 
-    depth = len(observed_sketch.sketch)
-    width = len(observed_sketch.sketch[0])
+    depth = len(previous_observed_sketch.sketch)
+    width = len(previous_observed_sketch.sketch[0])
     new_forecast_sketch = KAry_Sketch(depth,width)
-    if forecast_sketch != None:
+    if previous_forecast_sketch != None:
         for i in range(0,depth):
             for j in range(0,width):
-                new_forecast_sketch.sketch[i][j] = alpha*observed_sketch.sketch[i][j] + ((1-alpha)*forecast_sketch.sketch[i][j])
+                new_forecast_sketch.sketch[i][j] = (alpha*previous_observed_sketch.sketch[i][j]) + ((1-alpha)*previous_forecast_sketch.sketch[i][j])
         return new_forecast_sketch
     else:
-        return copy.deepcopy(observed_sketch)
+        return copy.deepcopy(previous_observed_sketch)
+
+def NSHW(previous_forecast_sketch,previous_observed_sketch,observed_sketch,previous_trend,previous_smoothing,alpha,beta):
+    """Uses the Non-Seasonal Holt-Winters Model to build the forecast sketch from the previous forecast, observed sketch, trend and smoothing
+
+    Parameters
+    ----------
+    previous_forecast_sketch : KAry_Sketch
+        A forecast sketch
+    previous_observed_sketch : KAry_Sketch
+        An observed sketch
+    observed_sketch : KAry_Sketch
+        An observed sketch
+    previous_trend : KAry_Sketch
+        The previous trend sketch
+    previous_smoothing : KAry_Sketch
+        The previous smoothing sketch
+    alpha : float
+        The alpha value to be used by the EWMA
+    beta : float
+        The beta value to be used by the EWMA
+smoothing
+    Returns
+    -------
+    KAry_Sketch
+        The forecast sketch
+    """
+
+    depth = len(previous_observed_sketch.sketch)
+    width = len(previous_observed_sketch.sketch[0])
+    smoothing_sketch = KAry_Sketch(depth,width)
+    trend_sketch = KAry_Sketch(depth,width)
+
+    #smoothing
+    if previous_forecast_sketch != None:
+        for i in range(0,depth):
+            for j in range(0,width):
+                smoothing_sketch.sketch[i][j] = (alpha*previous_observed_sketch.sketch[i][j]) + ((1-alpha)*previous_forecast_sketch.sketch[i][j])
+    else:
+        smoothing_sketch = copy.deepcopy(previous_observed_sketch)
+
+    #trend
+    if previous_forecast_sketch != None:
+        for i in range(0,depth):
+            for j in range(0,width):
+                trend_sketch.sketch[i][j] = (beta*(smoothing_sketch.sketch[i][j] - previous_smoothing.sketch[i][j])) + ((1-beta)*previous_trend.sketch[i][j])
+    else:
+        for i in range(0,depth):
+            for j in range(0,width):
+                trend_sketch.sketch[i][j] = observed_sketch.sketch[i][j] - previous_observed_sketch.sketch[i][j]
+
+    #Forecasting sketch
+
+    forecasting_sketch = KAry_Sketch(depth,width)
+    for i in range(0,depth):
+            for j in range(0,width):
+                forecasting_sketch.sketch[i][j] = trend_sketch.sketch[i][j] + smoothing_sketch.sketch[i][j]
+
+    return forecasting_sketch, smoothing_sketch, trend_sketch
 
 def forecast(forecast_sketch,sketch_list,alpha):
     """Calls a Forecasting Model to build the forecast sketch from the list of sketches observed in the past
@@ -241,7 +299,7 @@ def forecast(forecast_sketch,sketch_list,alpha):
 
     return EWMA(forecast_sketch,sketch_list[-2],alpha)
 
-def change(forecast_sketch,observed_sketch):
+def change(forecast_sketch,observed_sketch,T):
     """Constructs the forecast error sketch and chooses an alarm threshold, based on the second moment of the forecast error sketch.
 
     Parameters
@@ -261,7 +319,7 @@ def change(forecast_sketch,observed_sketch):
 
     depth = len(observed_sketch.sketch)
     width = len(observed_sketch.sketch[0])
-    T = 0.8
+
     new_error_sketch = KAry_Sketch(depth,width)
     for i in range(0,depth):
         for j in range(0,width):
@@ -274,16 +332,17 @@ def main():
     kary_width = 5462 #number of buckets in each row
     kary_epoch = 0.1 #seconds per epoch
     cur_epoch = None #current epoch
-    path = "traces/trace1.pcap"
-    #weights = [0.7,0.3] #keeps the weights given to the past past sketches in some forecasting models (must be the size of w)
-    alpha = 0.7 #alpha to be used by the EWMA
+    path = "traces/trace1.pcap" #path for the pcap file
+    alpha = 0.7 #alpha to be used by the EWMA and NSHW
+    beta = 0.7 #beta to be used by the NSHW
+    T = 0.1 #threshold used by the change detection module
     w = 1 #number of past sketches saved for forecast ( =1 for EWMA)
-    key_format = ["src","dst","dport","sport","proto"]
+    key_format = ["src","dst","dport","sport","proto"] #format of the key 
     keys = set()
 
     #-------------------------------------------- PROCESS INPUT --------------------------------------------#
-    short_options = "hd:w:e:a:k:p:"                                                                                                                                         
-    long_options = ["help", "depth=", "width=", "epoch=", "alpha=", "key=", "path="]
+    short_options = "hd:w:e:a:k:p:t:"                                                                                                                                         
+    long_options = ["help", "depth=", "width=", "epoch=", "alpha=", "key=", "path=", "thresh="]
     # Get full command-line arguments but the first
     argument_list = sys.argv[1:]
 
@@ -305,6 +364,7 @@ def main():
                     "--epoch          -e              positive float      0.1                            \n",
                     "--key            -k              \"opts...\"           \"src,dst,sport,dport,proto\"    \n",
                     "--path           -p              string              traces/trace1.pcap             \n",
+                    "--thresh         -t              positive float      0.1                            \n",
                     "--width          -w              positive integer    5462                           \n",
                     "--------------------------------------------------------------------------------------")
             exit()
@@ -324,14 +384,20 @@ def main():
             print("Updating key to", current_value)
             key = current_value.split(",")
         elif current_argument in ("-p", "--path"):
-            print("Updating key to", current_value)
+            print("Updating path to", current_value)
             path = current_value
+        elif current_argument in ("-t", "--thresh"):
+            print("Updating Threshold to", current_value)
+            T = float(current_value)
                                                                                                         
     #----------------------------------------------- | | -----------------------------------------------#
 
     forecast_sketch = None
     error_sketch = None
     threshold = None
+
+    trend_sketch = None #For use only with NSHW
+    smoothing_sketch = None #For use only with NSHW
     
     #initialize sketch list
     sketch_list = [] #keeps current sketch [-1] and w past sketches
@@ -340,7 +406,7 @@ def main():
 
     packets = PcapReader(path)
 
-    t = 1
+    control = 1
 
     for packet in packets:
         #-------------------------------------------- EXTRACT PACKET FIELDS --------------------------------------------#
@@ -359,7 +425,7 @@ def main():
         except:
             proto = 0
         try:
-            value = packet.len + len(packet)
+            value = packet.len
         except:
             value = len(packet)
 
@@ -376,7 +442,6 @@ def main():
             if elem == "proto":
                 key.append(str(proto))
 
-        value = len(packet)
         time = packet.time
         
         packet = {
@@ -393,14 +458,14 @@ def main():
         #Check if new packet is outside the current epoch
         if cur_epoch < packet["time"] - kary_epoch:
             #Only perform change detection if t >= 2
-            if t > 1:
+            if control > 1:
                 #FORECASTING
                 forecast_sketch = forecast(forecast_sketch,sketch_list,alpha)
 
                 #CHANGE DETECTION
-                error_sketch, threshold = change(forecast_sketch,sketch_list[-1])
+                error_sketch, threshold = change(forecast_sketch,sketch_list[-1],T)
+                print("Threshold =", threshold)
                 #error_sketch.SHOW()
-
                 for key in keys:
                     estimate = error_sketch.ESTIMATE(key)
                     if estimate  > threshold:
@@ -409,10 +474,10 @@ def main():
             print("Changing epoch ")
             cur_epoch = packet["time"]
 
-            if t == 1:
-                t = 2
+            if control == 1:
+                control = 2
 
-            #shift left
+            #shift left, deleting first [0]
             for i in range(0,w):
                 sketch_list[i] = copy.deepcopy(sketch_list[i+1])
 
